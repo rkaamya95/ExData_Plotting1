5b2e2dd61948336d5abea135ae2bc466273a4470
rkaamya95 <rkaamya95@gmail.com>
2020-07-23 16:53:54 -0700

    Second Plot

---
+++ plot2.R
@@ -0,0 +1,11 @@
+final<-read.csv("household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors = FALSE, skip=66636, nrows = 2880)
+colnames(final)<-names(d1)
+final$Date<-as.Date(final$Date, "%d/%m/%Y")
+final <- final[complete.cases(final),]
+datetime<-paste(final$Date, final$Time)
+dateTime <- setNames(dateTime, "DateTime")
+final<-cbind(datetime, final)
+final$datetime<-as.POSIXct(datetime)
+plot(final$Global_active_power~final$datetime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
+dev.copy(png, "plot2.png", width=480, height=480)
+dev.off()
