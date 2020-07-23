817be897b4bd87c3e1b77b9784bf4f7b3b8b6cc5
rkaamya95 <rkaamya95@gmail.com>
2020-07-22 19:54:01 -0700

    First Plot

---
+++ plot1.R
@@ -0,0 +1,11 @@
+new="https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(new, destfile="C:/Users/User/Downloads/eda.zip")
+dest<-"C:/Users/User/Downloads/eda"
+unzip("C:/Users/User/Downloads/eda.zip", files=NULL, overwrite = FALSE,junkpaths = FALSE, exdir=dest)
+setwd(dest)
+d1<-read.csv("household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors = FALSE)
+final_data<-read.csv("household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors = FALSE, skip=66637, nrows = 2880)
+colnames(final_data)<-names(d1)
+hist(final_data$Global_active_power, xlab = "Global Active Power(kilowatts)", col = "red", main="Global Active Power")
+dev.copy(png, "plot1.png", width=480, height=480)
+dev.off()
